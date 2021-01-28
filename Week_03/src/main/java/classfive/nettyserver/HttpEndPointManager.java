b41f79b8e76bd48c05cbed8f84a2be2d3b85aa55
natalie <22513892@qq.com>
2021-01-28 23:08:53 +0800

    1.update

--- Week_03/src/main/java/classfive/nettyserver/HttpEndPointManager.java
+++ Week_03/src/main/java/classfive/nettyserver/HttpEndPointManager.java
@@ -8,8 +8,8 @@ public class HttpEndPointManager {
     private static List<String> urls = new ArrayList<>();
 
     static {
-//        urls.add("http://localhost:8801");
-//        urls.add("http://localhost:8802");
+        urls.add("http://localhost:8801");
+        urls.add("http://localhost:8802");
         urls.add("http://localhost:8803");
     }
 
