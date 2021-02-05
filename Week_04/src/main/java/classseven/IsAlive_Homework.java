9412e6639ab865d87de07534f3a74730bb2d631f
natalie <22513892@qq.com>
2021-02-05 16:14:53 +0800

    第7课作业

--- Week_04/src/main/java/classseven/IsAlive_Homework.java
+++ Week_04/src/main/java/classseven/IsAlive_Homework.java
@@ -1,9 +1,6 @@
 package classseven;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
-public class ReadWriteLock_Homework {
+public class IsAlive_Homework {
 
     private static volatile int result;
 
@@ -13,24 +10,18 @@ public class ReadWriteLock_Homework {
 
         // 在这里创建一个线程或线程池，
         // 异步执行 下面方法
-        final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-        final Lock r = readWriteLock.readLock();
-        final Lock w = readWriteLock.writeLock();
-
 
         Thread thread = new Thread(() -> {
-            r.lock();
             result = Homework03.sum();
-            r.unlock();
-
         });
         thread.start();
-//        int result = sum(); //这是得到的返回值
 
+//        int result = sum(); //这是得到的返回值
+        while (thread.isAlive()) {
+
+        }
         // 确保  拿到result 并输出
-        w.lock();
         System.out.println("异步计算结果为："+result);
-        w.unlock();
 
         System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
 
